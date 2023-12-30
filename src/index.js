8812c97fd7e7d1147ebce146deffb974bc6d75ad
kalpesh mer <kalpeshmer447@gmail.com>
2023-12-30 15:38:13 +0530

    add registed controller

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path:'./env'
 })
-connectDB();
 
-
-
-
+connectDB()
+    .then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is Listening on PORT ${process.env.PORT}`);
+    })
+})
 
 
 
